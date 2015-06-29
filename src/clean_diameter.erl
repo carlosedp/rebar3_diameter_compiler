970d7c3026886e2a4fae047af9568e7e815109db
CarlosEDP <me@carlosedp.com>
2015-06-29 16:13:28 -0300

    Fix description

--- src/clean_diameter.erl
+++ src/clean_diameter.erl
@@ -24,7 +24,7 @@ init(State) ->
                                  {deps, ?DEPS},                 %% The list of dependencies
                                  {example, "rebar diameter clean"},   %% How to use the plugin
                                  {opts, []},                    %% list of options understood by the plugin
-                                 {short_desc, "clean the results of port compilation"},
+                                 {short_desc, "Clean compiled diameter files."},
                                  {desc, ""},
                                  {namespace, diameter}
                                 ]),
@@ -33,16 +33,52 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    rebar_api:info("Cleaning diameter compiled files...", []),
-    lists:foreach(fun(A) -> clean(State, A) end, rebar_state:project_apps(State)),
-    {ok, State}.
+    rebar_api:info("Cleaning compiled diameter files...", []),
+    Dir = rebar_state:dir(State),
+    io:format("Dir: ~p~n", [Dir]),
+
+    case rebar_app_discover:find_app(Dir, all) of
+        false ->
+            io:format("False~n"),
+            AllApps = rebar_state:project_apps(State) ++ rebar_state:all_deps(State),
+            io:format("AllApps: ~p~n", [AllApps]),
+            lists:foreach(fun(A) -> clean(A, State) end, AllApps);
+        {true, AppInfo} ->
+            io:format("True: ~p~n", [AppInfo]),
+            io:format("AppInfo: ~p~n", [AppInfo]),
+            clean(State, AppInfo)
+    end.
+
+    % {true, AppInfo} = rebar_app_discover:find_app(Dir, all),
+
+    % AllApps = rebar_state:project_apps(State) ++ rebar_state:all_deps(State),
+    % io:format("AllApps: ~p~n", [AllApps]),
+
+    % case rebar_app_utils:find(rebar_app_info:name(AppInfo), AllApps) of
+    %     {ok, AppInfo1} ->
+    %         %% Use the existing app info instead of newly created one
+
+    %         io:format("Case 1: ~p~n", [AppInfo1]),
+    %         clean(State, AppInfo1);
+    %     _ ->
+    %         io:format("Case 2: ~p~n", [AppInfo]),
+    %         clean(State, AppInfo)
+    % end.
+
+    % lists:foreach(fun(A) -> clean(State, A) end, rebar_state:project_apps(State)),
+    % {ok, State}.
 
 -spec format_error(any()) ->  iolist().
 format_error(Reason) ->
     io_lib:format("~p", [Reason]).
 
-clean(State, _AppFile) ->
-    file:set_cwd(rebar_app_info:dir(_AppFile)),
+clean(AppInfo, State) ->
+    Dir = rebar_state:dir(State),
+    io:format("Clean-Dir: ~p~n", [Dir]),
+    io:format("Clean-CWD-before: ~p~n", [file:get_cwd()]),
+
+    file:set_cwd(rebar_app_info:dir(AppInfo)),
+    io:format("Clean-CWD-after: ~p~n", [file:get_cwd()]),
     DiaOpts = rebar_state:get(State, dia_opts, []),
     IncludeEbin = proplists:get_value(include, DiaOpts, []),
     code:add_pathsz(["ebin" | IncludeEbin]),
